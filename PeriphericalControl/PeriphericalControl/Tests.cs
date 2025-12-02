69323fadc2b28523f0318c187e2b9d7b96483350
Timo74123 <Timoleon.hd@eduge.ch>
2025-12-02 15:39:12 +0100

    Update Test

---
+++ PeriphericalControl/PeriphericalControl/Tests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace PeriphericalControl
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void StickConfig_InsideDeadZone_ReturnsZero()
+        {
+            var stick = new StickConfig("Test");
+            stick.DeadZone = 0.2f;
+
+            float result = stick.Apply(0.1f);
+
+            Assert.AreEqual(0f, result, 0.0001f);
+        }
+
+        [TestMethod]
+        public void Profile_Constructor_CreatesEmptyList()
+        {
+            var profile = new Profile();
+            Assert.IsNotNull(profile.Sticks);
+            Assert.AreEqual(0, profile.Sticks.Count);
+        }
+
+        [TestMethod]
+        public void InputEvent_Constructor_StoresDateAndText()
+        {
+            DateTime now = DateTime.Now;
+            string msg = "Test";
+
+            var e = new InputEvent(now, msg);
+
+            Assert.AreEqual(now, e.Date);
+            Assert.AreEqual(msg, e.Text);
+        }
+    }
+}
